9133b0b7c36eba0dd26c4b2d0f006e378c16ff22
Saurabh700 <101572400+Saurabh700@users.noreply.github.com>
2022-05-08 19:05:40 +0530

    Add files via upload

---
+++ Yoox Header/header.js
@@ -0,0 +1,65 @@
+let downArrivals = document.getElementById("dropArrivals")
+    let dropDesigners = document.getElementById("dropDesigners")
+    let dropClothing = document.getElementById("dropClothing")
+    let dropShoes = document.getElementById("dropShoes")
+    let dropBags = document.getElementById("dropBags")
+    let dropYoox = document.getElementById("dropYoox")
+    let dropYooxygen = document.getElementById("dropYooxygen")
+    let dropCollaborations = document.getElementById("dropCollaborations")
+    let dropOffers = document.getElementById("dropOffers")
+    
+    let WArrivals = document.getElementById("WArrivals")
+    let WDesigners = document.getElementById("WDesigners")
+    let WClothing = document.getElementById("WClothing")
+    let WShoes = document.getElementById("WShoes")
+    let WBags = document.getElementById("WBags")
+    let WYoox = document.getElementById("WYoox")
+    let WYooxygen = document.getElementById("WYooxygen")
+    let WCollaborations = document.getElementById("WCollaborations")
+    let WBest = document.getElementById("WBest")
+
+        function newArrivals() {
+            downArrivals.classList.toggle("show")
+            WArrivals.classList.toggle("white")
+            WArrivals.classList.toggle("options:hover")
+        }
+        function Designers() {
+            dropDesigners.classList.toggle("show")
+            WDesigners.classList.toggle("white")
+            WDesigners.classList.toggle("options:hover")
+        }
+        function Clothing() {
+            dropClothing.classList.toggle("show")
+            WClothing.classList.toggle("white")
+            WClothing.classList.toggle("options:hover")
+        }
+        function Shoes() {
+            dropShoes.classList.toggle("show")
+            WShoes.classList.toggle("white")
+            WShoes.classList.toggle("options:hover")
+        }
+        function Bags() {
+            dropBags.classList.toggle("show")
+            WBags.classList.toggle("white")
+            WBags.classList.toggle("options:hover")
+        }
+        function Yoox() {
+            dropYoox.classList.toggle("show")
+            WYoox.classList.toggle("white")
+            WYoox.classList.toggle("options:hover")
+        }
+        function Yooxygen() {
+            dropYooxygen.classList.toggle("show")
+            WYooxygen.classList.toggle("white")
+            WYooxygen.classList.toggle("options:hover")
+        }
+        function Collaborations() {
+            dropCollaborations.classList.toggle("show")
+            WCollaborations.classList.toggle("white")
+            WCollaborations.classList.toggle("options:hover")
+        }
+        function Offers() {
+            dropOffers.classList.toggle("show")
+            WBest.classList.toggle("white")
+            WBest.classList.toggle("options:hover")
+        }
