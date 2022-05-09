c8d72e58d3e97bcfc5fdb22b29c3c214f4f1299c
MJaved9 <95mohdjaved@gmail.com>
2022-05-09 15:51:18 +0530

    cart and shoes navbar footer add

--- cart-script/cat.js
+++ cart-script/cat.js
@@ -131,3 +131,55 @@ let p_remove=(ele,i)=>{
     append(local_data,false)
     window.location.reload();
 }
+
+
+      
+// Black strip options
+let downArrivals = document.getElementById("dropArrivals")
+let dropDesigners = document.getElementById("dropDesigners")
+let dropClothing = document.getElementById("dropClothing")
+let dropShoes = document.getElementById("dropShoes")
+let dropBags = document.getElementById("dropBags")
+let dropYoox = document.getElementById("dropYoox")
+let dropYooxygen = document.getElementById("dropYooxygen")
+let dropCollaborations = document.getElementById("dropCollaborations")
+let dropOffers = document.getElementById("dropOffers")
+
+function newArrivals() {
+    downArrivals.classList.toggle("show")
+}
+function Designers() {
+    dropDesigners.classList.toggle("show")
+}
+function Clothing() {
+    dropClothing.classList.toggle("show")
+}
+function Shoes() {
+    dropShoes.classList.toggle("show")
+}
+function Bags() {
+    dropBags.classList.toggle("show")
+}
+function Yoox() {
+    dropYoox.classList.toggle("show")
+}
+function Yooxygen() {
+    dropYooxygen.classList.toggle("show")
+}
+function Collaborations() {
+    dropCollaborations.classList.toggle("show")
+}
+function Offers() {
+    dropOffers.classList.toggle("show")
+}
+
+
+
+
+/* Accordion Menu */
+const accordion = document.getElementsByClassName("contentBx");
+for (let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+    })
+}
