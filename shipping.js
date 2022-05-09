d16c0900568d3d573964fc627871be82f678c4a8
Pragati Tripathi <97211835+pragati-tripathi05@users.noreply.github.com>
2022-05-09 15:00:01 +0530

    Add files via upload

---
+++ shipping.js
@@ -0,0 +1,39 @@
+
+//let detailObj1 = {};
+let detailObj2 = {};
+
+// document.getElementById("form1").addEventListener("click",leftdata);
+// var details1 = JSON.parse(localStorage.getItem("shippingDetails1")) || [];
+// function leftdata()
+// {
+//     detailObj1 = {
+//           street : document.getElementById("street").value,
+//           city : document.getElementById("city").value,
+//           country : document.getElementById("country").value,
+//     };
+
+    
+//     details1.push(detailObj1);
+//     localStorage.setItem("shippingDetails1", JSON.stringify(details1));
+// }
+// console.log("detailObj1:",detailObj1);
+
+
+document.getElementById("form2").addEventListener("submit", data);
+var details2 = JSON.parse(localStorage.getItem("shippingDetails2")) || [];
+
+function data()
+{
+    event.preventDefault();
+    detailObj2 = {
+        name : document.getElementById("name").value,
+        phone : document.getElementById("phone").value,
+        state : document.getElementById("state").value,
+        zip : document.getElementById("zip").value,
+    };
+
+    console.log("detailObj2:",detailObj2);
+    details2.push(detailObj2);
+    localStorage.setItem("shippingDetails2", JSON.stringify(details2));
+    // window.location.href = "/layouts/payment.html";
+}
